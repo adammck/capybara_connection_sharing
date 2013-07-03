b7e6cbcdd1456c28ffc7ce8bb7769b608894f412
Adam Mckaig <adam.mckaig@gmail.com>
2013-07-03 13:54:10 -0400

    Remove unrelated stuff

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
 CapybaraConnectionSharing::Application.config.secret_key_base = '9b8e5991832895a7e5925f3f8ce0d97174e9dd2791fe6861caf3a9e3bbfaeffa83429aebaa33a6394710d7d0673a2858a0c12c9991618d95c21a11f126baed6b'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
 CapybaraConnectionSharing::Application.config.session_store :cookie_store, key: '_capybara_connection_sharing_session'
